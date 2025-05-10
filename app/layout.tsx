6b4d8faed1e20d3f29784032edb8b7e7470c7733
emircankaragoz <emircankaragoz@hotmail.com>
2025-05-10 09:36:48 +0300

    hepsi

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Viaport Kartal Yuvası - Çekiliş Kontrol Sistemi",
+  description: "KRTLYVS",
 };
 
 const geistSans = Geist({
